be6fe2e4146c44d2a92be1442e3c7bf57da8e8cc
HusaynShawer <hussienabdelmonhim@gmail.com>
2026-08-12 20:34:56 +0300

    feat(embeddings): enhance generate_embeddings function to accept input_type parameter feat(retriver): update retrieve_similar_products to use input_type for embedding generation and calculate score

--- Qena-Marketplace-Version3/backend/app/semantic_search/retriver.py
+++ Qena-Marketplace-Version3/backend/app/semantic_search/retriver.py
@@ -1,8 +1,9 @@
+import numpy as np
 from app.semantic_search.embeddings import generate_embeddings
 from app.semantic_search.embeddings_repo import EmbeddingsRepo
 
 async def retrieve_similar_products(session, query: str, top_k: int = 5):
-    query_embedding = (await generate_embeddings([query]))[0]
+    query_embedding = (await generate_embeddings([query], input_type="search_query"))[0]
     
     embeddings_repo = EmbeddingsRepo(session)
     similar_products = await embeddings_repo.similarity_search(query_embedding, top_k)
@@ -13,7 +14,10 @@ async def retrieve_similar_products(session, query: str, top_k: int = 5):
             "name": p.name,
             "description": p.description,
             "price": p.price,
-            "score": None  
+            "score": float(
+                np.dot(np.array(p.embeddings), np.array(query_embedding)) 
+                / (np.linalg.norm(np.array(p.embeddings)) * np.linalg.norm(np.array(query_embedding)))
+            )
         }
         for p in similar_products
     ]
